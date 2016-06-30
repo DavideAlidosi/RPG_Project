38a5ee0a58c8ffb5a3d1af43ad7dda0788989580
dbga_28 <dbga_28@dbga.local>
2016-06-30 16:41:49 +0200

    First popup test

---
+++ RPG Project/Assets/Script/UI/PopUp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUp : MonoBehaviour {
+
+    public bool isOver;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        Debug.Log("Over it!!!");
+        isOver == true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
